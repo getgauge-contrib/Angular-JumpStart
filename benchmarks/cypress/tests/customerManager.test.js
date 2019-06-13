337fa908e0a93b5590120fc49ed16f3c97b5cdd5
NivedhaSenthil <nivedhasenthil@gmail.com>
2019-06-13 10:00:57 +0530

    Initial benchmark setup with taiko tests

---
+++ benchmarks/cypress/tests/customerManager.test.js
@@ -0,0 +1,26 @@
+context('test groceryWorks', () => {
+    before(() => {
+      cy.visit('/');
+    });
+  
+    it('should navigate to right page', () => {
+    });
+
+    it('should filter customers', () => {
+        
+    });
+
+    it('should let user login')
+
+    it('should let adding customer')
+
+    it('should let edit customer')
+
+    it('should let viewing customer details')
+
+    it('should let view customer order')
+
+    it('should let navigate between pages to view customer')
+  }); 
+
+  

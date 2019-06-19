0f6cdde8589cbd754d7090eb4c9e87c048ee7b4f
NivedhaSenthil <nivedhasenthil@gmail.com>
2019-06-19 16:28:18 +0530

    update testcafe tests
    - use text search where ever possible

--- benchmarks/testcafe/tests/customerManager.test.js
+++ benchmarks/testcafe/tests/customerManager.test.js
@@ -8,11 +8,11 @@ test('should have the right title', async (t) => {
 });
 
 test('should let user login',async (t) => {
-    await t.click(Selector('body > cm-app-component > main > cm-navbar > nav > div > div > span > ul > li:nth-child(4) > a'));
+    await t.click(Selector('*').withExactText('Login'));
     await t.typeText(Selector('input[name="email"]'),'admin@customermanager.com');
     await t.typeText(Selector('input[name="password"]'),'password1234');
-    await t.click(Selector('button[type="submit"]'));
-    await t.expect(await Selector('body > cm-app-component > main > cm-navbar > nav > div > div > span > ul > li:nth-child(4) > a').innerText).eql('Logout');
+    await t.click(Selector('button').withText('Login'));
+    await t.expect(await Selector('*').withExactText('Logout').exists).ok();
 });
 
 test('should filter customers', async (t) => {
@@ -22,32 +22,32 @@ test('should filter customers', async (t) => {
 });
 
 test('should let adding customer', async (t) => {
-    await t.click(Selector('a[href="/customers/0/edit"]'));
+    await t.click(Selector('a').withText('New Customer'));
     await t.typeText(Selector('input[name="email"]'),'admin@customermanager.com');
     await t.typeText(Selector('input[name="password"]'),'password1234');
-    await t.click(Selector('button[type="submit"]'));
+    await t.click(Selector('button').withText('Login'));
     await t.typeText(Selector('input[name="firstName"]'),'first name');
     await t.typeText(Selector('input[name="lastName"]'),'last name');
     await t.typeText(Selector('input[name="address"]'),'address');
     await t.typeText(Selector('input[name="city"]'),'city');
     await t.click(Selector('select[name="state"]'));
     await t.click(await (Selector('select[name="state"]').find('option')).withText('Alabama'));
-    await t.click(Selector('button[type="submit"]'));
-    await t.click(Selector('cm-pagination li:nth-child(4) a'));
-    await t.expect(await (await Selector('.card .card-header .white').withText('First name Last name')).exists).ok();
+    await t.click(Selector('button').withText('Insert'));
+    await t.click(Selector('a').withText('3'));
+    await t.expect(await Selector('*').withText('First name Last name').exists).ok();
 });
 
 test('should let edit customer', async (t) => {
-    await t.click(Selector('cm-pagination li:nth-child(4) a'));
-    await t.click(Selector('a[href="/customers/23/details"]'));
-    await t.click(('a[href="/customers/23/edit"]'));
+    await t.click(Selector('a').withText('3'));
+    await t.click(Selector('a').withText('First name Last name'));
+    await t.click(Selector('a[routerlink="edit"]'));
     await t.typeText(Selector('input[name="email"]'),'admin@customermanager.com');
     await t.typeText(Selector('input[name="password"]'),'password1234');
-    await t.click(Selector('button[type="submit"]'));
+    await t.click(Selector('button').withText('Login'));
     await t.typeText(Selector('input[name="firstName"]'),'updated');
-    await t.click(('button[type="submit"]'));
-    await t.click(('a[href="/customers/23/details"]'));
-    await t.expect(await Selector('cm-customer-details h4').innerText).contains('First nameupdated');
+    await t.click(Selector('button').withText('Update'));
+    await t.click(Selector('a').withText('Customer Details'));
+    await t.expect(await Selector('*').withText('First nameupdated Last name').exists).ok();
 });
 
 test('should let view customer order', async (t) => {
